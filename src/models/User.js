8b9d9abffd9442121f0588780fa26a28a54e8581
TroublemakerSt <bugandcode@gmail.com>
2017-09-26 21:44:22 +0300

    Mailer + send confirmation email

--- src/models/User.js
+++ src/models/User.js
@@ -15,6 +15,7 @@ const schema = new mongoose.Schema(
     },
     passwordHash: { type: String, required: true },
     confirmed: { type: Boolean, default: false },
+    confirmationToken: { type: String, default: '' },
   },
   { timestamps: true },
 );
@@ -29,6 +30,14 @@ schema.methods.checkPass = function checkPass() {
 
 schema.methods.setPassword = function setPassword(password) {
   this.passwordHash = bcrypt.hashSync(password, 10);
+};
+
+schema.methods.setConfirmationToken = function setConfirmationToken() {
+  this.confirmationToken = this.generateJWT();
+};
+
+schema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
+  return `${process.env.HOST}/confirmation/${this.confirmationToken}`;
 };
 
 schema.methods.toAuthJSON = function toAuthJSON() {
