8b9f3cebd4e9ac0a055edfef4e57e8e7e48de7c2
TroublemakerSt <bugandcode@gmail.com>
2018-01-29 15:08:02 +0300

    Auth middleware was added and fixed bugs

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import authenticate from '../middlewares/authenticate';
 
 const router = express.Router();
+router.use(authenticate);
 
 router.get('/search', (req, res) => {
   res.json({
@@ -8,6 +10,16 @@ router.get('/search', (req, res) => {
       {
         goodreadsId: 1,
         title: '1984',
+        authors: 'Orwell',
+        covers: [
+          'https://ozon-st.cdn.ngenix.net/multimedia/1011499468.jpg',
+          'https://img1.labirint.ru/books/419735/big.jpg'
+        ],
+        pages: 198
+      },
+      {
+        goodreadsId: 2,
+        title: '1994',
         authors: 'Orwell',
         covers: ['https://images.gr-assets.com/books/13489905661/5470.jpg'],
         pages: 198
