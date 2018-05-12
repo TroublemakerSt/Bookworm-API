06dc73fb58e24f77a8c42788072330eb223671c0
TroublemakerSt <bugandcode@gmail.com>
2018-05-12 16:46:22 +0300

    Connect with GoodReads API

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,31 +1,32 @@
 import express from 'express';
+import request from 'request-promise';
+import { parseString } from 'xml2js';
 import authenticate from '../middlewares/authenticate';
 
 const router = express.Router();
 router.use(authenticate);
 
 router.get('/search', (req, res) => {
-  res.json({
-    books: [
-      {
-        goodreadsId: 1,
-        title: '1984',
-        authors: 'Orwell',
-        covers: [
-          'https://ozon-st.cdn.ngenix.net/multimedia/1011499468.jpg',
-          'https://img1.labirint.ru/books/419735/big.jpg'
-        ],
-        pages: 198
-      },
-      {
-        goodreadsId: 2,
-        title: '1994',
-        authors: 'Orwell',
-        covers: ['https://images.gr-assets.com/books/13489905661/5470.jpg'],
-        pages: 198
-      }
-    ]
-  });
+  request
+    .get(
+      `https://www.goodreads.com/search/index.xml?key=kynvUmrxfMUiS8hjWDk4QA&q=${
+        req.query.q
+      }`
+    )
+    .then(result =>
+      parseString(result, (err, goodreadsResult) =>
+        res.json({
+          books: goodreadsResult.GoodreadsResponse.search[0].results[0].work.map(
+            work => ({
+              goodreadsId: work.best_book[0].id[0]._,
+              title: work.best_book[0].title[0],
+              authors: work.best_book[0].author[0].name[0],
+              covers: [work.best_book[0].image_url[0]]
+            })
+          )
+        })
+      )
+    );
 });
 
 export default router;
