dd9f98887c826c54c3f693b3172496b6464d8326
TroublemakerSt <bugandcode@gmail.com>
2018-07-27 22:13:02 +0300

    mixin-deep upd

--- src/routes/books.js
+++ src/routes/books.js
@@ -9,9 +9,33 @@ router.use(authenticate);
 router.get('/search', (req, res) => {
   request
     .get(
-      `https://www.goodreads.com/search/index.xml?key=kynvUmrxfMUiS8hjWDk4QA&q=${
-        req.query.q
-      }`
+      `https://www.goodreads.com/search/index.xml?key=${
+        process.env.GOODREADS_KEY
+      }&q=${req.query.q}`
+    )
+    .then(result =>
+      parseString(result, (err, goodreadsResult) =>
+        res.json({
+          books: goodreadsResult.GoodreadsResponse.search[0].results[0].work.map(
+            work => ({
+              goodreadsId: work.best_book[0].id[0]._,
+              title: work.best_book[0].title[0],
+              authors: work.best_book[0].author[0].name[0],
+              covers: [work.best_book[0].image_url[0]]
+            })
+          )
+        })
+      )
+    );
+});
+
+router.get('/fetchPages', (req, res) => {
+  const goodreadsId = req.query.goodreadsId;
+  request
+    .get(
+      `https://www.goodreads.com/book/show.xml?key=${
+        process.env.GOODREADS_KEY
+      }&id=${goodreadsId}`
     )
     .then(result =>
       parseString(result, (err, goodreadsResult) =>
